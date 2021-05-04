ce08baa80a9f4aa7a3644ec88765c346d2f3a46e
Francisco Teixeira <francisco.lgteixeira@gmail.com>
2021-05-05 00:32:23 +0100

    Addition of type icons colors map and removal of Type closes #32

---
+++ lib/utils/typeIconsMap.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+
+var icons = { 
+  'bug':['assets/images/pokemonTypes/bug.svg', Color(0xFF92bc2c), Color(0xFF92bc2c)],
+
+  'dark':['assets/images/pokemonTypes/dark.svg', Color(0xFF595761), Color(0xFF595761)],
+
+  'dragon':['assets/images/pokemonTypes/dragon.svg', Color(0xFF0c69c8), Color(0xFF0c69c8)],
+
+  'electric':['assets/images/pokemonTypes/electric.svg', Color(0xFFf2d94e), Color(0xFFf2d94e)],
+
+  'fire':['assets/images/pokemonTypes/fire.svg', Color(0xFFfba54c), Color(0xFFfba54c)],
+
+  'fairy':['assets/images/pokemonTypes/fairy.svg', Color(0xFFee90e6), Color(0xFFee90e6)],
+
+  'fighting':['assets/images/pokemonTypes/fighting.svg', Color(0xFFd3425f), Color(0xFFd3425f)],
+
+  'flying':['assets/images/pokemonTypes/flying.svg', Color(0xFFa1bbec), Color(0xFFa1bbec)],
+
+  'ghost':['assets/images/pokemonTypes/ghost.svg', Color(0xFF5f6dbc), Color(0xFF5f6dbc)],
+
+  'grass':['assets/images/pokemonTypes/grass.svg', Color(0xFF5fbd58), Color(0xFF5fbd58)],
+
+  'ground':['assets/images/pokemonTypes/ground.svg', Color(0xFFda7c4d), Color(0xFFda7c4d)],
+
+  'ice':['assets/images/pokemonTypes/ice.svg', Color(0xFF75d0c1), Color(0xFF75d0c1)],
+
+  'normal':['assets/images/pokemonTypes/normal.svg', Color(0xFFa0a29f), Color(0xFFa0a29f)],
+
+  'poison':['assets/images/pokemonTypes/poison.svg', Color(0xFFb763cf), Color(0xFFb763cf)],
+
+  'psychic':['assets/images/pokemonTypes/psychic.svg', Color(0xFFfa8581), Color(0xFFfa8581)],
+
+  'rock':['assets/images/pokemonTypes/rock.svg', Color(0xFFc9bb8a), Color(0xFFc9bb8a)],
+
+  'steel':['assets/images/pokemonTypes/steel.svg', Color(0xFF5695a3), Color(0xFF5695a3)],
+
+  'water':['assets/images/pokemonTypes/water.svg', Color(0xFF539ddf), Color(0xFF539ddf)]
+};
